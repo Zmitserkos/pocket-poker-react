3b18259187916a87e17bdb1a303c94b939ba57a8
d.kosyanenko <d.kosyanenko@itransition.com>
2019-12-01 20:23:18 +0300

    dev: Connected the redux-thunk.

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './style.sass';
 import App from './components/app/app';
 import * as serviceWorker from './serviceWorker';
-import {createStore} from 'redux';
+import {applyMiddleware, createStore} from 'redux';
 import {Provider} from 'react-redux';
 import reducer from './reducers/reducer';
+import thunk from 'redux-thunk';
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
